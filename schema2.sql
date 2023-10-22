dc556c3747bdbef21930e0670461a9c0460cfc62
LeanBark <107157385+LeanBark@users.noreply.github.com>
2023-10-22 14:28:16 -0700

    cleaned up version with ALL values populated
    Removed unnecessary comments, populated item quantities so complete data sets are populated now
    Tested for clean import under foreign key checks enabled- submission ready?

--- schema2.sql
+++ schema2.sql
@@ -1,4 +1,3 @@
-
 -------------------------------------CREATE TABLES--------------------------------------
 
 
@@ -7,7 +6,6 @@ CREATE OR REPLACE TABLE Races (
     name varchar(255) NOT NULL,
     description text,
     PRIMARY KEY (race_id)
-    -- CONSTRAINT race_name UNIQUE (race_id,name)
 );
 
 CREATE OR REPLACE TABLE Classes (
@@ -15,29 +13,25 @@ CREATE OR REPLACE TABLE Classes (
     name varchar(255) UNIQUE NOT NULL,
     description text,
     PRIMARY KEY (class_id)
-    -- CONSTRAINT class_name UNIQUE (class_id,name)
 );
 
 CREATE OR REPLACE TABLE ItemTypes (
     item_type_id int NOT NULL AUTO_INCREMENT,
     name varchar(255) UNIQUE NOT NULL,
     PRIMARY KEY (item_type_id)
-    -- CONSTRAINT item_type UNIQUE (item_type_id, name)
 );
 
 CREATE OR REPLACE TABLE Actions (
     action_id int NOT NULL AUTO_INCREMENT,
     name varchar(255) UNIQUE NOT NULL,
     PRIMARY KEY (action_id)
-    -- CONSTRAINT action_name UNIQUE (action_id, name)
 );
 
 CREATE OR REPLACE TABLE EventDifficulties (
     difficulty_id int NOT NULL AUTO_INCREMENT,
-    value int UNIQUE NOT NULL, -- placed unique identifier here instead
+    value int UNIQUE NOT NULL,
     description text,
     PRIMARY KEY (difficulty_id)
-    -- CONSTRAINT difficulty_type UNIQUE (difficulty_class_id, value)
 );
 
 CREATE OR REPLACE TABLE Items (
@@ -45,10 +39,9 @@ CREATE OR REPLACE TABLE Items (
     name varchar(255) UNIQUE NOT NULL,
     quantity int NOT NULL DEFAULT 1,
     item_type_id int,
-    PRIMARY KEY (item_id),  -- composite key 
+    PRIMARY KEY (item_id),
     FOREIGN KEY (item_type_id) REFERENCES ItemTypes(item_type_id)
     ON DELETE SET NULL
-    -- CONSTRAINT item_identity UNIQUE (name, item_type_id)
 );
 
 CREATE OR REPLACE TABLE Characters (
@@ -69,14 +62,6 @@ CREATE OR REPLACE TABLE Characters (
     FOREIGN KEY (class_id) REFERENCES Classes (class_id)
     ON DELETE SET NULL
 );
-
--- CREATE OR REPLACE TABLE CharacterDetails (
---     id int NOT NULL AUTO_INCREMENT,
---     character_id int NOT NULL,
---     race_id int NOT NULL,
---     class_id int NOT NULL,
---     PRIMARY KEY (id)
--- );
 
 CREATE OR REPLACE TABLE SkillChecks (
     skill_check_id int NOT NULL AUTO_INCREMENT,
@@ -191,7 +176,6 @@ VALUES
 
 -------------------POPULATING CHARACTERS TABLE------------------------------
 
--- VERSION 1: hard-coded FK race and class id values
 INSERT INTO Characters (
     name,
     level,
@@ -212,84 +196,22 @@ VALUES
     ("Fred", 10, 10, 17, 14, 9, 16, 10, 
     7, 11);
 
--- VERSION 2: Subqueries used to select race_id, class_id by specified values (commented out)
--- INSERT INTO Characters (
---     name,
---     level,
---     strength,
---     dexterity,
---     constitution,
---     intelligence,
---     wisdom,
---     charisma,
---     race_id,
---     class_id
--- )
--- VALUES
---     (
---         (SELECT character_id FROM Characters WHERE Characters.name = "beginner"),
---         (SELECT race_id FROM Races WHERE Races.name = "Human"), 
---         (SELECT class_id FROM Classes WHERE Classes.name = "Cleric")
-        
---     ),
---     (
---         (SELECT character_id FROM Characters WHERE Characters.name = "intermediate"),
---         (SELECT race_id FROM Races WHERE Races.name = "Tiefling"), 
---         (SELECT class_id FROM Classes WHERE Classes.name = "Warlock")
-        
---     ),
---     (
---         (SELECT character_id FROM Characters WHERE Characters.name = "expert"), 
---         (SELECT race_id FROM Races WHERE Races.name = "Half-Elf"),
---         (SELECT class_id FROM Classes WHERE Classes.name = "Monk")
---     );
 
 -------------------------POPULATING ITEMS TABLE----------------------------------
 
----VERSION 1: Hard-coded FK item_type_id values
 INSERT INTO Items (
     name,
+    quantity,
     item_type_id
 )
 VALUES 
-    ("Elixer of Cloud Giant", 1),
-    ("Boots of Speed", 3),
-    ("Staff of Fireball", 4),
-    ("Scroll of Charm", 2),
-    ("Necklace of Intelligence", 5),
-    ("Ring of Invisibility", 6);
+    ("Elixer of Cloud Giant", 2, 1),
+    ("Boots of Speed", 1, 3),
+    ("Staff of Fireball", 1, 4),
+    ("Scroll of Charm", 3, 2),
+    ("Necklace of Intelligence", 1, 5),
+    ("Ring of Invisibility", 1, 6);
 
-
--- Version 2: Select subqueries to retrieve FK values (commented out)
--- INSERT INTO Items (
---     name,
---     item_type_id
--- )
--- VALUES 
-    --     (
-    --     "Elixer of Cloud Giant",
-    --     (SELECT item_type_id FROM ItemTypes WHERE ItemTypes.name = "Potion")
-    -- ),
-    -- (
-    --     "Boots of Speed",
-    --     (SELECT item_type_id FROM ItemTypes WHERE ItemTypes.name = "Armor")
-    -- ),
-    -- (
-    --     "Staff of Fireball",
-    --     (SELECT item_type_id FROM ItemTypes WHERE ItemTypes.name = "Weapon")
-    -- ),
-    -- (
-    --     "Scroll of Charm Humanoid",
-    --     (SELECT item_type_id FROM ItemTypes WHERE ItemTypes.name = "Scroll")
-    -- ),
-    -- (
-    --     "Necklace of Intelligence",
-    --     (SELECT item_type_id FROM ItemTypes WHERE ItemTypes.name = "Amulet")
-    -- ),
-    -- (
-    --     "Ring of Invisibility",
-    --     (SELECT item_type_id FROM ItemTypes WHERE ItemTypes.name = "Ring")
-    -- );
 
 -----------------------------POPULATING SKILLCHECKS TABLE-------------------
 
@@ -304,23 +226,8 @@ VALUES
     ("Attempt to Initmidate Bronze Dragon", 20, 6),
     ("Attempt to Sneak Past Spectator", 5, 5);
 
--- Version 2: select subqueries (commented out)
--- INSERT INTO SkillChecks (
---     description,
---     roll_result,
---     difficulty_class_id
--- )
--- VALUES
---     ("Attempt to Cross Icy Bridge", 1,
---     SELECT difficulty_class_id FROM DifficultyClasses WHERE DifficultyClasses.name = "Moderate"),
---     ("Attempt to Initmidate Bronze Dragon", 20,
---     SELECT difficulty_class_id FROM DifficultyClasses WHERE DifficultyClasses.name = "Very Difficult"),
---     ("Attempt to Sneak Past Spectator", 5,
---     SELECT difficulty_class_id FROM DifficultyClasses WHERE DifficultyClasses.name = "Difficult");
-
 -----------------------------POPULATING SKILLCHECKDETAILS-------------------------------------
 
--- Version 1: Hard-coded FK values for actions, items, characters, skill_checks
 INSERT INTO SkillCheckDetails (
     action_id,
     character_id,
@@ -332,22 +239,7 @@ VALUES
     (6, 2, 4, 2),
     (3, 3, 6, 3);
 
-
---Version 2: Select subqueries to insert FKs (commented out)
--- INSERT INTO SkillCheckDetails (
---     action_id,
---     character_id,
---     item_id,
---     skill_check_id
--- )
--- VALUES
---     (SELECT action_id FROM Actions WHERE Actions.name = "Class-Based Action"),
---     (SELECT character_id FROM Characters WHERE Characters.name = "expert"),
---     (SELECT item_id FROM Items WHERE Items.name = "Boots of Speed"),
---     (SELECT skill_check_id FROM SkillChecks WHERE SkillChecks.description = "Attempt to Cross Icy Bridge");
-
-
-----------------------------SAMPLE DATA SETS--------------------------------------------------------------------
+---------------------------SAMPLE DATA SETS--------------------------------------------------------------------
 
 -- SCENARIOS FOR PRESET DATA SAMPLE VALUES------------------------------
 
@@ -361,6 +253,7 @@ VALUES
 -- SkillCheckDetails.action_id = 2 -> "Dash"
 -- SkillCheckDetails.item_id = 2 -> "Boots of Speed" 
         ---> Items.item_type_id = 3 -> "Armor"
+        ---> Items.quantity = 1
 -- SkillCheckDetails.skill_check_id = 1 
         ---> SkillChecks.difficulty_id = 4 -> "Difficult"
         ---> SkillChecks.description = ""Attempt to Cross Icy Bridge""
@@ -377,6 +270,7 @@ VALUES
 -- SkillCheckDetails.action_id = 6 -> "Class-Action"
 -- SkillCheckDetails.item_id = 4 -> "Scroll of Charm" 
         ---> Items.item_type_id = 2 -> "Scroll"
+        ---> Items.quantity = 3
 -- SkillCheckDetails.skill_check_id = 2 
         ---> SkillChecks.difficulty_id = 6 -> "Extreme"
         ---> SkillChecks.description = ""Attempt to Initmidate Bronze Dragon""
@@ -393,77 +287,8 @@ VALUES
 -- SkillCheckDetails.action_id = 3 -> "Hide"
 -- SkillCheckDetails.item_id = 6 -> "Ring of Invisibility" 
         ---> Items.item_type_id = 6 -> "Ring"
+        ---> Items.quantity = 1
 -- SkillCheckDetails.skill_check_id = 3 
         ---> SkillChecks.difficulty_id = 5 -> "Very Difficult"
         ---> SkillChecks.description = ""Attempt to Sneak Past Spectator""
 
-
--------------------------------------------------TEST QUERIES----------------------------------------------------
-
--- --- Query determines whether Items and ItemTypes are properly associated and return expected value pairs
--- SELECT Items.name AS "Item name", ItemTypes.name AS "Item Type" 
--- FROM Items INNER JOIN ItemTypes ON Items.item_type_id = ItemTypes.item_type_id;
-
-
--- --- Query determines whether Characters, Races, Items return expected value pairs
--- SELECT Characters.name AS "Character Name", Characters.strength, Characters.wisdom, Races.name AS "Race",
--- Classes.name AS "Class" 
--- FROM ((Characters 
--- INNER JOIN Races ON Characters.race_id = Races.race_id)
--- INNER JOIN Classes ON Characters.class_id = Classes.class_id);
-
-
--- --- Query determines whether Characters, SkillChecks, Actions, Items all form correct value pairs within SkillCheckDetails Table
--- SELECT Characters.name AS "Character", SkillChecks.description AS "Skill Check Event", Items.name AS "Item used", Actions.name AS "Action used" 
--- FROM ((((SkillCheckDetails
--- INNER JOIN Characters ON SkillCheckDetails.character_id = Characters.character_id)
--- INNER JOIN Items ON SkillCheckDetails.item_id = Items.item_id)
--- INNER JOIN Actions ON SkillCheckDetails.action_id = Actions.action_id)
--- INNER JOIN SkillChecks ON SkillCheckDetails.skill_check_id = SkillChecks.skill_check_id);
-
-
--- --- Query determines whether attributes associated with different entities within SkillCheckDetails are correctly returned
--- SELECT Characters.race_id AS "Race ID", Characters.class_id AS "Class ID", SkillChecks.roll_result AS "Roll Value", Items.item_type_id AS "Item type ID"
--- FROM (((SkillCheckDetails
--- INNER JOIN Characters ON SkillCheckDetails.character_id = Characters.character_id)
--- INNER JOIN Items ON SkillCheckDetails.item_id = Items.item_id)
--- INNER JOIN SkillChecks ON SkillCheckDetails.skill_check_id = SkillChecks.skill_check_id);
-
--- SELECT Characters.name AS "Character", Races.name AS "Race", Classes.name AS "Class" 
--- FROM ((Characters
--- INNER JOIN Races ON Characters.race_id = Races.race_id)
--- INNER JOIN Classes ON Characters.class_id = Classes.class_id);
-
--- SELECT Items.name AS "Item name", ItemTypes.name AS "Item Type"
--- FROM Items INNER JOIN ItemTypes ON Items.item_type_id = ItemTypes.item_type_id;
-
-
--- -- DELETION OPERATIONS ---------------------
--- -- NOTE: FOR DELETE/UPDATE OPERATIONS, you must select by primary key values and set foreign key checks off
-
--- -- Query deletes a Character and checks to make sure tables with relations were not deleted
--- DELETE FROM Characters WHERE Characters.character_id = 1; -- delete character
--- SELECT * FROM Characters; -- confirm deletion from Characters table
--- SELECT * FROM Races; -- check for alterations in Races table 
--- SELECT * FROM Classes; -- check for alterations in Classes table
-
--- -- Query updates a Character's name, race_id, and class_id then checks to see if changes were properly made
--- UPDATE Characters SET Characters.name = "Bob", Characters.race_id = 2, Characters.class_id = 4 WHERE Characters.character_id = 2;
-
-
--- -- Query deletes an item from Items table
--- DELETE FROM Items WHERE Items.item_id = 1;
--- select * from Items; -- confirm deletion of item 
--- select * from ItemTypes; -- check for alterations in ItemsTypes table
-
--- clear database
--- set foreign_key_checks = 0;
--- drop table Actions;
--- drop table Characters;
--- drop table Classes;
--- drop table EventDifficulties;
--- drop table ItemTypes;
--- drop table Items;
--- drop table Races;
--- drop table SkillCheckDetails;
--- drop table SkillChecks;
