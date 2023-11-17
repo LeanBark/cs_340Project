448f999b755eb7e42dd40787d79247a47a9dd64c
Jonathan Reuter <reuterjo@flip4.engr.oregonstate.edu>
2023-11-16 21:42:54 -0800

    Added rolling dice and updated the navbar style

---
+++ ajax-handlebar-ui/public/js/roll_dice.js
@@ -0,0 +1,16 @@
+function rollDice() {
+    let mainImg = document.getElementById("main-img");
+    let mainImgButton = document.getElementById("main-img-button");
+    let source = mainImg.getAttribute("src");
+    mainImgButton.disabled = true;
+    mainImg.setAttribute("class", "rotate-img")
+    if (source == "./img/d20_critical_failure.png"){
+        mainImg.setAttribute("src", "./img/d20_critical_success.png");
+    } else {
+        mainImg.setAttribute("src", "./img/d20_critical_failure.png");
+    }
+    setTimeout(() => {
+        mainImg.setAttribute("class", "")
+        mainImgButton.disabled = false;
+    }, "550");
+}
